6d7e55a170bc098e2e0231cb4e9041f599dfd4cb
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-09-18 10:13:16 +0700

    change

--- testWeather-app/src/components/Weather.jsx
+++ testWeather-app/src/components/Weather.jsx
@@ -1,8 +1,22 @@
 import './Weather.css'
+import search_icon from '../components/assets/search.png'
+import clear_icon from '../components/assets/clear.png'
+import cloud_icon from '../components/assets/cloud.png'
+import drizzle_icon from '../components/assets/drizzle.png'
+import humid_icon from '../components/assets/humidity.png'
+import rain_icon from '../components/assets/rain.png'
+import snow_icon from '../components/assets/snow.png'
+import wind_icon from '../components/assets/wind.png'
+
 const Weather = () => {
     return (
        <>
-        <div>hello</div>
+        <div className='container'>
+            <div className='top-bar'>
+                <input type="text" className='cityInput' placeholder='Search...'/>
+                <div className='search-icon'></div>
+            </div>
+        </div>
        </>
     );
 };
