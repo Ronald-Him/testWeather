93b622d8ee0fb05f00234aca28f440ac984b2258
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-09-18 10:39:11 +0700

    change

--- testWeather-app/src/components/Weather.jsx
+++ testWeather-app/src/components/Weather.jsx
@@ -18,6 +18,27 @@ const Weather = () => {
                     <img src={search_icon} alt="" />
                 </div>
             </div>
+            <div className='weather-img'>
+                <img src={cloud_icon} alt="" />
+            </div>
+            <div className='weather-tmp'>10<sup>o</sup>C</div>
+            <div className='weather-location'>Phnom Penh</div>
+            <div className='data-container'>
+                <div className='element'>
+                    <img src={humid_icon} alt="" />
+                    <div className='data'>
+                        <div className='humidnity-percent'>64%</div>
+                        <div className='text'>Humidnity</div>
+                    </div>
+                </div>
+                <div className='element'>
+                    <img src={wind_icon} alt="" />
+                    <div className='data'>
+                        <div className='wind-speed'>18 km/h</div>
+                        <div className='text'>Wind Speed</div>
+                    </div>
+                </div>
+            </div>
         </div>
        </>
     );
