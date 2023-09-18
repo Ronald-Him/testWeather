78acb2ce73b10e636fcc6e81bdc3643dbbcc6abe
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-09-18 11:03:42 +0700

    get into api

--- testWeather-app/src/components/Weather.jsx
+++ testWeather-app/src/components/Weather.jsx
@@ -11,13 +11,21 @@ import wind_icon from '../components/assets/wind.png'
 const Weather = () => {
 
     let api_key = "1017b0d8ddc253536421c752d8f3bd1b";
+    const search = () => {
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value===""){
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&appid=${api_key}`
+    }
+
 
     return (
        <>
         <div className='container'>
             <div className='top-bar'>
                 <input type="text" className='cityInput' placeholder='Search...'/>
-                <div className='search-icon'>
+                <div className='search-icon' onClick={()=>{search()}}>
                     <img src={search_icon} alt="" />
                 </div>
             </div>
